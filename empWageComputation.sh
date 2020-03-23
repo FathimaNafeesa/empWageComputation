a776d2a30b95fd4f934d61a873f473a7c14217c2
fathima <fathimanafeesavy@gmail.com>
2020-03-23 11:57:09 +0530

    [fathima]added switch statements and converted previous prgram to function

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,7 +1,7 @@
 #WelcomeMessage
 echo "                                                                                         Welcome to Employee Wage Computation Program                                     "
 #branch UC1
-
+function checkemployeepresentorabsent {
 echo "Enter employee name"
 read employeename
 
@@ -12,9 +12,9 @@ then
 else
 	echo "Employee $employeename is absent"
 fi
-
+}
 #branch UC2
-
+function calculatedailyemployeewage {
 WAGE_PER_HOUR=20
 FULL_TIME_HOUR=8
 
@@ -37,5 +37,17 @@ PART_TIME_HOUR=4
 
 parttimewage=$(($WAGE_PER_HOUR * $PART_TIME_HOUR))
 
-echo "The part time employee $employeename has a daily wage of $parttimewage Rupee
+echo "The part time employee $employeename has a daily wage of $parttimewage Rupee"
+}
+#branch UC4
 
+option=$((RANDOM % 4))
+
+case $option in
+    1) checkemployeepresentorabsent
+    ;;
+    2) calculatedailyemployeewage
+    ;;
+    3) parttimeemployeewage
+    ;;
+esac
