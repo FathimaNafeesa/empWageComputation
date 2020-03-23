66e8b130491683c2ec96ae7c18f2519a4476b81c
fathima <fathimanafeesavy@gmail.com>
2020-03-23 14:29:41 +0530

    [fathima]Welcome Message

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -13,6 +13,7 @@ else
 	echo "Employee $employeename is absent"
 fi
 }
+
 #branch UC2
 function calculatedailyemployeewage {
 WAGE_PER_HOUR=20
@@ -46,20 +47,21 @@ esac
 #branch UC5
 #branch UC6
 FULL_TIME_HOUR_PER_MONTH=100
+WORKING_DAYS_PER_MONTH=20
 workeddays=1
 workedhours=1
-while [ $workeddays -le WORKING_DAYS_PER_MONTH ] -o [ $workedhours -le $FULL_TIME_HOUR_PER_MONTH ]
+while [ $workeddays -le $WORKING_DAYS_PER_MONTH ] && [ $workedhours -le $FULL_TIME_HOUR_PER_MONTH ]
 do
     totalwage=$(($WAGE_PER_HOUR * $workedhours)) 
-    ((workedhours++))
+    (($workedhours++))
     totalwagepermonth=$(($WAGE_PER_HOUR * $workeddays * $FULL_TIME_HOUR)) 
-    ((workeddays++))
+    (($workeddays++))
 done
  echo "The monthly wage is $totalwagepermonth"
  echo "The daily wage is $totalwage"
- 
+ #branch UC7
 function calculateworkinghours {
-    workedhours=$((totalwagepermonth / ($WAGE_PER_HOUR * $FULL_TIME_HOUR)
+    workedhours=$(($totalwagepermonth / $WAGE_PER_HOUR * $FULL_TIME_HOUR))
     echo "The working hours is $workedhours"
 }
 calculateworkinghours
