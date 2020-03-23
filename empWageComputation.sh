538f9f5b233786c325e2d490588a2dc5a6c7185a
fathima <fathimanafeesavy@gmail.com>
2020-03-23 12:55:47 +0530

    [fathima]calculate wage for a month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -23,21 +23,13 @@ totalwage=$(($WAGE_PER_HOUR * $FULL_TIME_HOUR))
 echo "The employee $employeename has a daily wage of $totalwage Rupees"
 }
 
-#branch parttimeemployeewage
+#branch UC3
 function parttimeemployeewage {
 PART_TIME_HOUR=4
 
 parttimewage=$(($WAGE_PER_HOUR * $PART_TIME_HOUR))
 
 echo "The part time employee $employeename has a daily wage of $parttimewage Rupees"
-
-#branch UC3
-
-PART_TIME_HOUR=4
-
-parttimewage=$(($WAGE_PER_HOUR * $PART_TIME_HOUR))
-
-echo "The part time employee $employeename has a daily wage of $parttimewage Rupee"
 }
 #branch UC4
 
@@ -51,3 +43,18 @@ case $option in
     3) parttimeemployeewage
     ;;
 esac
+#branch UC5
+
+FULL_TIME_HOUR_PER_MONTH=100
+workeddays=1
+workedhours=1
+while [ $workeddays -le WORKING_DAYS_PER_MONTH ] -o [ $workedhours -le $FULL_TIME_HOUR_PER_MONTH ]
+do
+    totalwage=$(($WAGE_PER_HOUR * $workedhours)) 
+    ((workedhours++))
+    totalwagepermonth=$(($WAGE_PER_HOUR * $workeddays * $FULL_TIME_HOUR)) 
+    ((workeddays++))
+done
+ echo "The monthly wage is $totalwagepermonth"
+ echo "The daily wage is $totalwage"
+ 
