f2cbdf9995cc731e27f2a9e47aad75380c2c5276
fathima <fathimanafeesavy@gmail.com>
2020-03-23 11:16:41 +0530

    [fathima]To check employee present or absent

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,14 @@
 #WelcomeMessage
 echo "                                                                                         Welcome to Employee Wage Computation Program                                     "
+#branch checkemployeepresentorabsent
+
+echo "Enter employee name"
+read employeename
+
+attendance=$((RANDOM % 2))
+if [ attendance = 1 ]
+then
+	echo "Employee $employeename is present"
+else
+	echo "Employee $employeename is absent"
+fi
