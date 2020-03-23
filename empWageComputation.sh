a5b1553145862c21179c5080405ebfe0d6dee5ac
fathima <fathimanafeesavy@gmail.com>
2020-03-23 11:27:17 +0530

    [fathima]To calculate daily employee wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,6 +1,6 @@
 #WelcomeMessage
 echo "                                                                                         Welcome to Employee Wage Computation Program                                     "
-#branch checkemployeepresentorabsent
+#branch UC1
 
 echo "Enter employee name"
 read employeename
@@ -12,3 +12,21 @@ then
 else
 	echo "Employee $employeename is absent"
 fi
+
+#branch UC2
+
+WAGE_PER_HOUR=20
+FULL_TIME_HOUR=8
+
+totalwage=$(($WAGE_PER_HOUR * $FULL_TIME_HOUR))
+ 
+echo "The employee $employeename has a daily wage of $totalwage Rupees"
+}
+
+#branch parttimeemployeewage
+function parttimeemployeewage {
+PART_TIME_HOUR=4
+
+parttimewage=$(($WAGE_PER_HOUR * $PART_TIME_HOUR))
+
+echo "The part time employee $employeename has a daily wage of $parttimewage Rupees"
